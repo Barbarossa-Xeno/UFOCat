d4853e1a5423fc417b300bbea7f2d8ac77330a71
Barbarossa_Xeno <060812ez.tm@gmail.com>
2025-10-22 08:17:06 +0900

    コメント官僚

--- cat_action.hpp
+++ cat_action.hpp
@@ -10,14 +10,19 @@ namespace cact
 	/// @brief `CatObject::cross()` のシグネチャなど
 	namespace cross
 	{
-		
+		/// @brief `CatObject::cross(Duration period, uint32 count)` を表すタプル
 		using _0 = std::tuple<Duration, uint32>;
+		/// @brief `CatObject::cross(Duration period)` を表すタプル
 		using _1 = std::tuple<Duration>;
 
+		/// @brief `CatObject::cross()` の引数シグネチャの全てのタプルを含む variant
 		using TSignatures = std::variant<_0, _1>;
 
+		/// @brief `TSignatures` が含む型の数
 		constexpr size_t Count = std::variant_size_v<TSignatures>;
 
+		/// @brief `CatObject::cross()` の有効な引数シグネチャの条件
+		/// @tparam 登録されたシグネチャとの比較対象
 		template <typename T>
 		concept ValidSignature =
 			(
@@ -26,21 +31,34 @@ namespace cact
 			);
 	}
 
+	/// @brief `CatObject::appear()` のシグネチャなど
 	namespace appear
 	{
+		/// @brief `CatObject::appear(Duration period, EasingFunction fadeInFunc, Duration fadeIn, EasingFunction fadeOutFunc, Duration fadeOut, const Rect &range)` を表すタプル
 		using _0 = std::tuple<Duration, EasingFunction, Duration, EasingFunction, Duration, Rect>;
+		/// @brief `CatObject::appear(Duration period, Duration fadeIn, Duration fadeOut, const Rect &range)` を表すタプル
 		using _1 = std::tuple<Duration, Duration, Duration, Rect>;
+		/// @brief `CatObject::appear(Duration period, EasingFunction fadeFunc, Duration fade, const Rect &range)` を表すタプル
 		using _2 = std::tuple<Duration, EasingFunction, Duration, Rect>;
+		/// @brief `CatObject::appear(Duration period, Duration fade, const Rect &range)` を表すタプル
 		using _3 = std::tuple<Duration, Duration, Rect>;
+		/// @brief `CatObject::appear(Duration period, EasingFunction fadeInFunc, Duration fadeIn, EasingFunction fadeOutFunc, Duration fadeOut)` を表すタプル
 		using _4 = std::tuple<Duration, EasingFunction, Duration, EasingFunction, Duration>;
+		/// @brief `CatObject::appear(Duration period, Duration fadeIn, Duration fadeOut)` を表すタプル
 		using _5 = std::tuple<Duration, Duration, Duration>;
+		/// @brief `CatObject::appear(Duration period, EasingFunction fadeFunc, Duration fade)` を表すタプル
 		using _6 = std::tuple<Duration, EasingFunction, Duration>;
+		/// @brief `CatObject::appear(Duration period, Duration fade)` を表すタプル
 		using _7 = std::tuple<Duration, Duration>;
 
+		/// @brief `CatObject::appear()` の引数シグネチャの全てのタプルを含む variant
 		using TSignatures = std::variant<_0, _1, _2, _3, _4, _5, _6, _7>;
 
+		/// @brief `TSignatures` が含む型の数
 		constexpr size_t Count = std::variant_size_v<TSignatures>;
 
+		/// @brief `CatObject::appear()` の有効な引数シグネチャの条件
+		/// @tparam 登録されたシグネチャとの比較対象
 		template <typename T>
 		concept ValidSignature =
 			(
@@ -55,17 +73,26 @@ namespace cact
 			);
 	}
 
+	/// @brief /// @brief `CatObject::appearFromEdge()` のシグネチャなど
 	namespace appearFromEdge
 	{
+		/// @brief `CatObject::appearFromEdge(Duration period, EasingFunction inFunc, Duration in, EasingFunction outFunc, Duration out, const std::array<double, 4> &overflow)` を表すタプル
 		using _0 = std::tuple<Duration, EasingFunction, Duration, EasingFunction, Duration, std::array<double, 4>>;
+		/// @brief `CatObject::appearFromEdge(Duration period, Duration in, Duration out, const std::array<double, 4> &overflow)` を表すタプル
 		using _1 = std::tuple<Duration, Duration, Duration, std::array<double, 4>>;
+		/// @brief `CatObject::appearFromEdge(Duration period, EasingFunction inAndOutFunc, Duration inAndOut, const std::array<double, 4> &overflow)` を表すタプル
 		using _2 = std::tuple<Duration, EasingFunction, Duration, std::array<double, 4>>;
+		/// @brief `CatObject& appearFromEdge(Duration period, Duration inAndOut, const std::array<double, 4> &overflow)` を表すタプル
 		using _3 = std::tuple<Duration, Duration, std::array<double, 4>>;
 
+		/// @brief `CatObject::appearFromEdge()` の引数シグネチャの全てのタプルを含む variant
 		using TSignatures = std::variant<_0, _1, _2, _3>;
 
+		/// @brief `TSignatures` が含む型の数
 		constexpr size_t Count = std::variant_size_v<TSignatures>;
 
+		/// @brief `CatObject::appearFromEdge()` の有効な引数シグネチャの条件
+		/// @tparam 登録されたシグネチャとの比較対象
 		template <typename T>
 		concept ValidSignature =
 			(
@@ -76,12 +103,15 @@ namespace cact
 			);
 	}
 
+	/// @brief `CatObject` 内全ての行動系メソッドのシグネチャ（猫を行動させるパラメータ）が入る万能型
 	using Generic = std::variant<
 		std::monostate,
 		cross::_0, cross::_1,
 		appear::_0, appear::_1, appear::_2, appear::_3, appear::_4, appear::_5, appear::_6, appear::_7,
 		appearFromEdge::_0, appearFromEdge::_1, appearFromEdge::_2, appearFromEdge::_3>;
 
+	/// @brief `CatObject` 内いずれかの行動系メソッドに有効なシグネチャの条件
+	/// @tparam 有効なシグネチャとの比較対象を入れる
 	template <typename T>
 	concept ValidSignature =
 	(
