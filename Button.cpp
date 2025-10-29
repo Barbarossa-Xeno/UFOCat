7257629c69cdfa9cf8b810982905029fa22ed01c
Barbarossa_Xeno <060812ez.tm@gmail.com>
2025-10-29 18:18:11 +0900

    ButtonRegionなんかいらなかった

--- Button.cpp
+++ Button.cpp
@@ -2,21 +2,20 @@
 
 namespace UFOCat::Component
 {
-	ButtonRegion &Button::set(const Vec2& position, const Font& font, const String& text, bool isEnabled, const Vec2& padding) const
+	Button &Button::set(const Font& font, const String& text, bool isEnabled, const Vec2& padding) const
 	{
-		m_selfRegion = std::make_unique<ButtonRegion>(position, font, text, isEnabled, padding);
+		m_selfRegion = std::make_unique<Button>(font, text, isEnabled, padding);
 		return *m_selfRegion;
 	}
 
-	Button::Button(const Vec2 &position, const Font &font, const String &text, bool isEnabled, const Vec2 &padding)
-		: m_position(position)
-		, m_font(font)
+	Button::Button(const Font &font, const String &text, bool isEnabled, const Vec2 &padding)
+		: m_font(font)
 		, m_text(text)
 		, m_isEnabled(isEnabled)
 		, m_padding(padding)
-		// フォントの描画領域を特定して、パディング分を足したサイズで Rect を作成
-		, m_rect{ m_position, (m_font(m_text).region(m_position).size + m_padding) }
-	{ }
+		// フォントの描画領域 (左上(0, 0)の位置) を特定して、パディング分を足したサイズで Rect を作成
+		, m_region{ m_font(m_text).region().size + m_padding }
+	{}
 
 	bool Button::isPressed() const
 	{
@@ -26,36 +25,97 @@ namespace UFOCat::Component
 		Button const* ptr = m_selfRegion ? m_selfRegion.get() : this;
 
 		// マウスカーソルがボタンの上にある場合
-		if (ptr->m_isEnabled and ptr->m_rect.mouseOver())
+		if (ptr->m_isEnabled and ptr->m_region.mouseOver())
 		{
 			// マウスカーソルを手の形にする
 			Cursor::RequestStyle(CursorStyle::Hand);
 		}
 
 		// ボタンが押されたら true を返す
-		return (ptr->m_isEnabled and ptr->m_rect.leftClicked());
+		return (ptr->m_isEnabled and ptr->m_region.leftClicked());
 	}
 
-	void ButtonRegion::draw() const
+	void Button::draw(const Vec2 &position) const
 	{
-		const RoundRect roundRect = m_rect.rounded(6);
+		// 範囲を指定位置にずらしたうえで、描画用の角丸長方形を作成
+		// mutable だから、できること。
+		const RoundRect roundRect = m_region.moveBy(position).rounded(6);
 
 		// 影と背景を描く
 		roundRect.drawShadow(Vec2{ 2, 2 }, 12, 0)
 			.draw(ColorF{ 0.9, 0.8, 0.6 });
 
 		// 枠を描く
-		m_rect.stretched(-3).rounded(3)
+		m_region.stretched(-3).rounded(3)
 			.drawFrame(2, ColorF{ 0.4, 0.3, 0.2 });
 
 		// テキストを描く
-		m_font(m_text).drawAt(m_rect.center(), ColorF{ 0.4, 0.3, 0.2 });
-
+		m_font(m_text).drawAt(roundRect.center(), ColorF{ 0.4, 0.3, 0.2 });
+		
 		// 無効の場合は
 		if (not m_isEnabled)
 		{
 			// グレーの半透明を重ねる
 			roundRect.draw(ColorF{ 0.8, 0.8 });
 		}
+	}
+
+	void Button::draw(const Arg::topCenter_<Vec2> &position) const
+	{
+		// 中央基準位置から自分の長さの半分ずらして左上位置とする、yはそのまま
+		double x = position->x - m_region.w / 2;
+		return draw(Vec2{ x, position->y });
+	}
+
+	void Button::draw(const Arg::topRight_<Vec2> &position) const
+	{
+		// 右端基準位置から自分の長さ分ずらして左上位置とする、yはそのまま
+		double x = position->x - m_region.w;
+		return draw(Vec2{ x, position->y });
+	}
+
+	void Button::draw(const Arg::leftCenter_<Vec2> &position) const
+	{
+		// 左端中央基準位置から自分の高さの半分ずらす、xはそのまま
+		double y = position->y - m_region.h / 2;
+		return draw(Vec2{ position->x, y });
+	}
+
+	void Button::draw(const Arg::rightCenter_<Vec2> &position) const
+	{
+		// 右端中央基準位置から自分の高さの半分ずらす、xはそのまま
+		double y = position->y - m_region.h / 2;
+		return draw(Vec2{ position->x - m_region.w, y });
+	}
+
+	void Button::draw(const Arg::bottomLeft_<Vec2> &position) const
+	{
+		// 左下基準位置から自分の高さ分ずらす、xはそのまま
+		double y = position->y - m_region.h;
+		return draw(Vec2{ position->x, y });
+	}
+
+	void Button::draw(const Arg::bottomCenter_<Vec2> &position) const
+	{
+		// 下中央基準位置から自分の高さ分と半分ずらす、xは中央基準位置から自分の長さの半分ずらす
+		double x = position->x - m_region.w / 2;
+		double y = position->y - m_region.h;
+		return draw(Vec2{ x, y });
+	}
+
+	void Button::draw(const Arg::bottomRight_<Vec2> &position) const
+	{
+		// 右下基準位置から自分の高さ分ずらす、xは右端基準位置から自分の長さ分ずらす
+		double x = position->x - m_region.w;
+		double y = position->y - m_region.h;
+		return draw(Vec2{ x, y });
+	}
+
+	void Button::drawAt(const Vec2 &position) const
+	{
+		// 中央基準位置から自分の長さと高さの半分ずらして左上位置とする
+		double x = position.x - m_region.w / 2;
+		double y = position.y - m_region.h / 2;
+		return draw(Vec2{ x, y });
 	}
 }
--- Component.hpp
+++ Component.hpp
@@ -2,13 +2,10 @@
 
 namespace UFOCat::Component
 {
-	struct ButtonRegion;
-
+	/// @brief ボタン
 	class Button
 	{
 	protected:
-
-		Vec2 m_position;
 
 		Font m_font;
 
@@ -19,50 +16,112 @@ namespace UFOCat::Component
 		Vec2 m_padding = { 30.0, 10.0 };
 
 		/// @brief ボタンの描画領域
-		RectF m_rect;
+		/// @remarks mutable にして描画メソッドでの値の更新を許可する
+		mutable RectF m_region;
 
 	private:
 		/// @brief ボタンのデータから生成する ButtonRegion のポインタ
 		/// @remarks ButtonRegion のメソッドチェーンと、Button のインスタンスを参照したときに、
 		/// 同じ ButtonRegion を返すために使用するので、mutable にしている
-		mutable std::unique_ptr<ButtonRegion> m_selfRegion = nullptr;
+		/// なお、ButtonRegion には継承されない（参照一貫性を保つため）
+		mutable std::unique_ptr<Button> m_selfRegion = nullptr;
 
 	public:
 
 		/// @brief ボタンの各種パラメータを一括で設定する
-		/// @param position 左上の位置
 		/// @param font テキストに使うフォント
 		/// @param text テキスト
 		/// @param isEnabled 有効かどうか
 		/// @param padding ボタンの内側余白 (デフォルトは (30, 10))
-		ButtonRegion &set(const Vec2& position, const Font& font, const String& text, bool isEnabled = true, const Vec2& padding = { 30.0, 10.0 }) const;
+		Button &set(const Font& font, const String& text, bool isEnabled = true, const Vec2& padding = { 30.0, 10.0 }) const;
 
+		/// @brief デフォルトコンストラクタ
 		Button() = default;
 
 		// TODO: アイコンを引数にとれるようにしたい
-		/// @brief ボタンを左上位置に、フォントサイズを基準に描画し、押されたかどうかを返す
-		/// @param position 左上の位置
+		// TODO: フォントサイズを指定できるようにしてもいいかも
+		/// @brief ボタンを初期化する
 		/// @param font テキストに使うフォント
 		/// @param text テキスト
 		/// @param isEnabled 有効かどうか
 		/// @param padding ボタンの内側余白 (デフォルトは (30, 10))
 		/// @note https://siv3d.github.io/ja-jp/tutorial2/button/ を参考に改変
-		Button(const Vec2 &position, const Font &font, const String &text, bool isEnabled = true, const Vec2 &padding = { 30.0, 10.0 });
+		Button(const Font &font, const String &text, bool isEnabled = true, const Vec2 &padding = { 30.0, 10.0 });
 
 		/// @brief ボタンが押されたかを返す
 		/// @return 押されたら `true`
 		bool isPressed() const;
+
+		// TODO: 色の指定もほしいかも
+		/// @brief 左上位置を指定してボタンを描画する
+		/// @param position 左上位置
+		void draw(const Vec2& position) const;
+
+		/// @brief 中央上位置を指定してボタンを描画する
+		/// @param position 中央上位置
+		void draw(const Arg::topCenter_<Vec2>& position) const;
+
+		/// @brief 右上位置を指定してボタンを描画する
+		/// @param position 右上位置
+		void draw(const Arg::topRight_<Vec2>& position) const;
+
+		/// @brief 中央左位置を指定してボタンを描画する
+		/// @param position 中央左上位置
+		void draw(const Arg::leftCenter_<Vec2>& position) const;
+
+		/// @brief 中央右位置を指定してボタンを描画する
+		/// @param position 中央右位置
+		void draw(const Arg::rightCenter_<Vec2>& position) const;
+
+		/// @brief 左下位置を指定してボタンを描画する
+		/// @param position 左下位置
+		void draw(const Arg::bottomLeft_<Vec2>& position) const;
+
+		/// @brief 中央下位置を指定してボタンを描画する
+		/// @param position 中央下位置
+		void draw(const Arg::bottomCenter_<Vec2>& position) const;
+
+		/// @brief 右下位置を指定しボタンを描画する
+		/// @param position 右下位置
+		void draw(const Arg::bottomRight_<Vec2>& position) const;
+
+		/// @brief 中央位置を指定してボタンを描画する
+		/// @param position 中央位置
+		void drawAt(const Vec2& position) const;
 	};
 
-	struct ButtonRegion : public Button
+	// TODO: 後で作る！！
+	class Scrollable
 	{
-		ButtonRegion() : Button() {}
+	protected:
 
-		ButtonRegion(const Vec2 &position, const Font &font, const String &text, bool isEnabled = true, const Vec2 &padding = { 30.0, 10.0 })
-			: Button(position, font, text, isEnabled, padding)
-		{ }
+		Font m_font;
 
-		/// @brief ボタンを描画する
-		void draw() const;
+		String m_text;
+
+		Vec2 m_padding = { 40.0, 20.0 };
+
+		mutable RectF m_region;
+
+		RectF m_innerRegion;
+
+	public:
+
+		Scrollable() = default;
+
+		Scrollable(const SizeF &size, const Font &font, const String &text, const Vec2 &padding = { 40.0, 20.0 });
+
+		void update();
+	};
+
+	struct ScrollableRegion : public Scrollable
+	{
+		ScrollableRegion();
+
+		ScrollableRegion(const SizeF &size, const Font &font, const String &text, const Vec2 &padding = { 40.0, 20.0 });
+
+		void draw(const Vec2 &position) const;
+
+		void drawAt(const Vec2 &position) const;
 	};
 }
