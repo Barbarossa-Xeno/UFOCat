b8af7646414e4990fc1a76cab496c3dc727f1bac
Barbarossa_Xeno <060812ez.tm@gmail.com>
2025-10-29 18:18:23 +0900

    のちのち作成

---
+++ Scrollable.cpp
@@ -0,0 +1,41 @@
+﻿# include "Component.hpp"
+
+namespace UFOCat::Component
+{
+	Scrollable::Scrollable(const SizeF &size, const Font &font, const String &text, const Vec2 &padding)
+		: m_font(font)
+		, m_text(text)
+		, m_padding(padding)
+		, m_region{ size }
+		, m_innerRegion{ padding, (size - padding) }
+	{}
+
+	void Scrollable::update()
+	{
+		if (m_innerRegion.mouseOver())
+		{
+			m_innerRegion.moveBy(0.0, Mouse::Wheel());
+		}
+	}
+
+	ScrollableRegion::ScrollableRegion() : Scrollable() {}
+
+	ScrollableRegion::ScrollableRegion(const SizeF &size, const Font &font, const String &text, const Vec2 &padding)
+		: Scrollable(size, font, text, padding)
+	{}
+
+	void ScrollableRegion::draw(const Vec2 &position) const
+	{
+		const RectF current = m_region.movedBy(position);
+		current.draw(Palette::Aliceblue);
+
+		m_innerRegion.draw();
+
+		m_font(m_text).draw(m_innerRegion, Palette::Black);
+
+		/*current.left().draw(m_padding.x, Palette::Aliceblue);
+		current.right().draw(m_padding.x, Palette::Aliceblue);
+		current.top().draw(m_padding.y, Palette::Aliceblue);
+		current.bottom().draw(m_padding.y, Palette::Aliceblue);*/
+	}
+}
