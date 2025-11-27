2a380e1d50aeab7097d3a1eaca0d8fca1db4b3c2
Barbarossa_Xeno <060812ez.tm@gmail.com>
2025-11-28 07:50:35 +0900

    オーディオ、フォントアセット更新追加

---
+++ AudioList.hpp
@@ -0,0 +1,41 @@
+﻿# pragma once
+
+namespace UFOCat::Util::AudioList
+{
+	namespace BGM
+	{
+		const String Title = U"Title";
+
+		const String Level01 = U"Level01";
+
+		const String Level02 = U"Level02";
+	}
+
+	namespace SE
+	{
+		const String Open = U"Open";
+
+		const String Close = U"Close";
+
+		const String Announce = U"Annonnce";
+
+		// TODO: 音源取り忘れ中
+		const String CountDown = U"CountDown";
+
+		const String TimeUp = U"TimeUp";
+
+		const String Correct = U"Correct";
+
+		const String Incorrect = U"Incorrect";
+
+		const String CountUpScore = U"CountUpScore";
+
+		const String FinishScore = U"FinishScore";
+
+		const String Cat01 = U"Cat01";
+
+		const String Cat02 = U"Cat02";
+
+		const String CatAngry = U"CatAngry";
+	}
+}
